31251dc0ce5ae2b88ce3eb2319b20f61319e430a
Omotayo Olukoya <tolukoya@berkeley.edu>
2015-02-10 04:27:59 -0400

    first

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -5,11 +5,44 @@ class HangpersonGame
 
   # Get a word from remote "random word" service
 
+  attr_reader:word,:guesses,:wrong_guesses,:win,:loss
   def self.get_random_word
     require 'uri'
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
+  def initialize(word)
+    @word = word
+    @guesses =''
+    @wrong_guesses = ''
+    @dict = {}
+    @word.each_char do |w| 
+      @dict[w] = w
+    end
+
+  end
+
+  def guess(w)
+    if !w.match(/^[[:alpha:]]$/)
+      raise ArgumentError
+    end
+
+    if @dict.has_key?(w)
+      @guesses += w
+      @dict.delete(w)
+      @win = @dict.empty?
+      true
+    else
+      @wrong_guesses+=w
+      @loss =@wrong_guesses.length >= @word.length
+      false
+    end
+  end
+
+
+
+  
+
 
 end
